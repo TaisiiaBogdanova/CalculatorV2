300b5d406ea044412e0b89f2f650f01a369c28ae
TaisiiaBogdanova <taisiiabogdanovaqa@gmail.com>
2024-11-07 11:15:34 +0100

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Constant {
+    public static final char SIGN_PLUS = '+';
+    public static final char SIGN_MINUS = '-';
+    public static final char SIGN_MULTI = '*';
+    public static final char SIGN_DIV = '/';
+    public static final char SIGN_PROC = '%';
+    public static final String ANS_YES = "Y";
+    public static final String ANS_NO = "N";
+}
